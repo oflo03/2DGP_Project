08b675f1ebedc4e9962a3e9a8391eb2173faf5c9
oflo03 <samleebw@naver.com>
2023-10-22 00:12:09 +0900

    add main system loop

--- The Mountain Man/main.py
+++ The Mountain Man/main.py
@@ -1,9 +1,35 @@
 from pico2d import *
 
-open_canvas()
+def handle_events():
+    global running
 
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            #game_world.handle_event(event)
+            pass
+
+def update_world():
+    #game_world.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    #game_world.render()
+    update_canvas()
+
+open_canvas()
+#create_world()
 running = True
 while running:
-    print('canvas opened!')
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
 
 close_canvas()
