416fd7dfcf535f2cffe4eddae8d7bc6318c7da28
oflo03 <samleebw@naver.com>
2023-10-28 12:29:25 +0900

    modify BackImage class

--- The Mountain Man/play_state.py
+++ The Mountain Man/play_state.py
@@ -4,21 +4,21 @@ import world
 
 
 class BackImage:
-    def __init__(self, filename, x, y):
+    def __init__(self, filename, pos=(game_framework.WIDTH // 2, game_framework.HEIGHT // 2), scale=(1, 1)):
         self.image = load_image(filename)
-        self.x, self.y = x, y
+        self.pos = pos
+        self.scale = scale
+        world.add_object(self)
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(self.x, self.y, self.image.w * game_framework.WIDTH / 800,
-                        self.image.h * game_framework.HEIGHT / 600)
+        self.image.draw(self.pos[0], self.pos[1])
 
 
 def enter():
-    grass = BackImage('grass.png', 400 * game_framework.WIDTH / 800, 30 * game_framework.HEIGHT / 600)
-    world.add_object(grass)
+    BackImage('grass.png')
 
 
 def exit():
