d8d3e12171a8e193b772435a068c1b24d1611832
oflo03 <samleebw@naver.com>
2023-11-12 23:39:04 +0900

    create climber class

--- The Mountain Man/climber.py
+++ The Mountain Man/climber.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import game_framework
 
-class Player:
+class Climber:
     def __init__(self, pos=(game_framework.WIDTH // 2, game_framework.HEIGHT // 2)):
         self.image = load_image('resources/player.png')
         self.pos = pos
