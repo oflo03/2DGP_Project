ce4bfbb46166c469b518426b7c3884745a4f8dbe
oflo03 <samleebw@naver.com>
2023-11-14 22:36:50 +0900

    add limbs rotate

--- The Mountain Man/climber.py
+++ The Mountain Man/climber.py
@@ -8,18 +8,18 @@ class Climber:
         self.body = load_image('resources/body.png')
         self.limbs = load_image('resources/limbs.png')
         self.pos = [pos[0], pos[1]]
-        self.click_points = [(-40, 90), (40, 90)]
+        self.click_points = [[-70, 20], [70, 20], [-30, -170], [30, -170]]
         self.locked_points = []
-        self.selected_point = None
+        self.selected_index = -1
         self.dir = [0, 0]
-        self.radian = 0
-        self.rad_dir = 0
+        self.limbs_radian = [0.0, 0.0, 0.0, 0.0]
+        self.limbs_preset = [(-70, 70), (70, 70), (-30, -120), (30, -120)]
         world.add_object(self, 2)
 
     def update(self):
         if len(self.locked_points):
-            self.locked_points[0][0]
-        if not self.selected_point and 0 == len(self.locked_points):
+            pass
+        if self.selected_index == -1 and 0 == len(self.locked_points):
             self.dir[1] += -0.01
             if self.pos[1] <= 100:
                 if self.dir[1] < -0.1:
@@ -30,12 +30,20 @@ class Climber:
                 self.dir[1] = 1
             for i in [0, 1]:
                 self.pos[i] += self.dir[i]
-        self.radian += self.rad_dir
+        for i in range(0, 4):
+            dot_product = (self.click_points[i][1] - self.limbs_preset[i][1]) * -1
+            magnitude_product = math.sqrt((self.click_points[i][0] - self.limbs_preset[i][0]) ** 2 + (
+                    self.click_points[i][1] - self.limbs_preset[i][1]) ** 2)
+            if magnitude_product == 0:
+                break
+            self.limbs_radian[i] = math.acos(dot_product / magnitude_product) * (1 if (self.click_points[i][0] - self.limbs_preset[i][0]) >= 0 else -1)
 
     def draw(self):
-        # self.limbs.composite_draw(-0.3, '', self.pos[0] - 60, self.pos[1] + 70)
-        # self.limbs.composite_draw(0.3, '', self.pos[0] + 60, self.pos[1] + 70)
-        self.body.composite_draw(self.radian, '', self.pos[0], self.pos[1])
+        for i in range(0, 4):
+            self.limbs.composite_draw(self.limbs_radian[i], '', self.pos[0] + self.limbs_preset[i][0],
+                                      self.pos[1] + self.limbs_preset[i][1])
+
+        self.body.draw(self.pos[0], self.pos[1])
         for cp in self.click_points:
             draw_rectangle(self.pos[0] + cp[0] - 20, self.pos[1] + cp[1] - 20, self.pos[0] + cp[0] + 20,
                            self.pos[1] + cp[1] + 20)
@@ -45,18 +53,24 @@ class Climber:
             for cp in self.click_points:
                 if abs(self.pos[0] + cp[0] - e.x) < 20 and abs(
                         self.pos[1] + cp[1] - (game_framework.HEIGHT - e.y)) < 20:
-                    self.selected_point = cp
-                    self.pos = [e.x - self.selected_point[0], (game_framework.HEIGHT - e.y) - self.selected_point[1]]
+                    self.selected_index = self.click_points.index(cp)
+                    self.click_points[self.selected_index] = [e.x - self.pos[0],
+                                                              (game_framework.HEIGHT - e.y) - self.pos[1]]
                     self.locked_points.clear()
         elif e.type == SDL_MOUSEBUTTONUP:
-            if self.selected_point:
+            if self.selected_index >= 0:
                 for hold in world.world[1]:
-                    if abs(self.pos[0] + self.selected_point[0] - hold.pos[0]) < 20 and abs(
-                            self.pos[1] + self.selected_point[1] - hold.pos[1]) < 20:
-                        self.locked_points.append(self.selected_point)
-                        self.pos = [hold.pos[0] - self.selected_point[0], hold.pos[1] - self.selected_point[1]]
-                self.selected_point = None
+                    if abs(self.pos[0] + self.click_points[self.selected_index][0] - hold.pos[0]) < 20 and abs(
+                            self.pos[1] + self.click_points[self.selected_index][1] - hold.pos[1]) < 20:
+                        self.locked_points.append(self.click_points[self.selected_index])
+                        self.pos = [hold.pos[0] - self.click_points[self.selected_index][0],
+                                    hold.pos[1] - self.click_points[self.selected_index][1]]
+                self.selected_index = -1
                 self.dir = [0, 0]
         elif e.type == SDL_MOUSEMOTION:
-            if self.selected_point:
-                self.pos = [e.x - self.selected_point[0], (game_framework.HEIGHT - e.y) - self.selected_point[1]]
+            if self.selected_index >= 0:
+                self.click_points[self.selected_index] = [e.x - self.pos[0],
+                                                          (game_framework.HEIGHT - e.y) - self.pos[1]]
+                if math.sqrt((e.x - self.limbs_preset[self.selected_index][0]) ** 2 + (
+                        (game_framework.HEIGHT - e.y) - self.limbs_preset[self.selected_index][1]) ** 2) > 60:
+                    pass
