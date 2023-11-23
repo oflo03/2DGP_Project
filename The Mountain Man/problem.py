fe9ac6247a572916dbfca70a72517412966836ca
oflo03 <samleebw@naver.com>
2023-11-23 12:55:59 +0900

    modify pr system

--- The Mountain Man/problem.py
+++ The Mountain Man/problem.py
@@ -1,18 +1,14 @@
 from hold import Hold
 
 
-def change(str, idx):
-    data = int(str[idx]) * 1000
-    data += int(str[idx + 1]) * 100
-    data += int(str[idx + 2]) * 10
-    data += int(str[idx + 3])
-    return data
-
-
 class Problem:
     def __init__(self, filename):
         self.holds = []
         with open(filename, 'r') as file:
-            str = file.read()
-        for i in range(0, len(str) // 12):
-            self.holds.append(Hold(int(str[i * 12]), (change(str, i * 12 + 2), change(str, i * 12 + 7))))
+            while True:
+                line = file.readline()
+                if not line:
+                    break
+                else:
+                    h_type, h_x, h_y = line.split(" ")
+                    self.holds.append(Hold(int(h_type), (int(h_x), int(h_y))))
