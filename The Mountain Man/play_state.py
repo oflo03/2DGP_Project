c1fb27f6d9a537f267758a639c729b8b7d70f4c7
oflo03 <samleebw@naver.com>
2023-11-25 22:50:46 +0900

    Update play_state.py

--- The Mountain Man/play_state.py
+++ The Mountain Man/play_state.py
@@ -2,25 +2,36 @@ from pico2d import *
 
 import cursor
 import game_framework
+import menu_state
+import option_state
 import world
 from back_image import BackImage
 from climber import Climber
 from problem import Problem
 
 climber = None
-
+p = None
+is_clear = False
+p_num = 0
+bgm = None
 
 def enter():
-    global climber
+    global climber, p, is_clear, bgm
     BackImage('resources/background.png')
-    Problem('problems/test.pr')
+    p = Problem()
+    if p_num != -1:
+        p.init('problems/world'+str(p_num)+'.json')
     climber = Climber()
+    is_clear = False
+    if bgm == None:
+        bgm = load_music('resources/bgm2.mp3')
+        bgm.set_volume(option_state.volume)
+        bgm.repeat_play()
 
 
 def exit():
-    global climber
+    global climber, p, bgm
     world.clear()
-    pass
 
 
 def pause():
@@ -32,25 +43,43 @@ def resume():
 
 
 def handle_events():
+    global bgm, is_clear
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
+                bgm.stop()
+                bgm = None
+                menu_state.bgm.repeat_play()
                 game_framework.pop_state()
+            if event.key == SDLK_n:
+                is_clear = True
         elif event.type == SDL_MOUSEMOTION:
             cursor.x, cursor.y = event.x, game_framework.HEIGHT - event.y
         climber.handle_events(event)
 
 
 def update():
+    global p_num, bgm
     world.update()
-    pass
+    if is_clear:
+        if p_num == -1 or p_num == 10:
+            bgm.stop()
+            bgm = None
+            menu_state.bgm.repeat_play()
+            game_framework.pop_state()
+            return
+        exit()
+        p_num += 1
+        enter()
 
 
 def draw():
+    global p
     clear_canvas()
     world.draw()
+    p.draw()
     cursor.image.draw(cursor.x, cursor.y, 64, 64)
     update_canvas()
