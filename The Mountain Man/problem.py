26d3b4d8e2020b0cd7165291feca46b3c7829800
oflo03 <samleebw@naver.com>
2023-12-01 22:51:50 +0900

    Update problem.py

--- The Mountain Man/problem.py
+++ The Mountain Man/problem.py
@@ -1,14 +1,30 @@
+import world
 from hold import Hold
+import json
+from button import Button
 
 
 class Problem:
-    def __init__(self, filename):
+    def __init__(self):
         self.holds = []
-        with open(filename, 'r') as file:
-            while True:
-                line = file.readline()
-                if not line:
-                    break
-                else:
-                    h_type, h_x, h_y = line.split(" ")
-                    self.holds.append(Hold(int(h_type), (int(h_x), int(h_y))))
+
+    def init(self, filename):
+        with open(filename, 'r') as f:
+            problem_data_list = json.load(f)
+            for data in problem_data_list:
+                h = Hold()
+                h.__dict__.update(data)
+                self.holds.append(h)
+
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            problem_data_list = [{"num": h.num, "pos": h.pos} for h in self.holds]
+            json.dump(problem_data_list,f)
+
+    def delete(self):
+        self.holds.clear()
+        world.world[1].clear()
+
+    def draw(self):
+        if len(self.holds) > 0:
+            Button.font.draw(self.holds[len(self.holds)-1].pos[0] - 60, self.holds[-1].pos[1] + 50, "TOP", (0, 0, 0))
